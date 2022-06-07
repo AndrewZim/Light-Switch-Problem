185735641813ff80bd5f70ee50d785f27c80f8f0
Andrew Zimmerman <andrew@abcsoftek.com>
2022-06-07 15:59:23 -0400

    formatting

--- Light Switch Problem/LightSwitch.asm
+++ Light Switch Problem/LightSwitch.asm
@@ -4,11 +4,11 @@
 public light_switch
 light_switch proc switches
         mov     edx, esp
-        mov     eax, 0
+        xor     eax, eax
         mov     ecx, switches
         mov     ebx, ecx
         shr     ecx, 2
-init:   push eax
+init:   push    eax
         loop    init            ;initialize the memory space
         mov     eax, 0          ;which person
 personloop: inc eax             ;next person
@@ -23,19 +23,18 @@ doneswitch: mov ecx, ebx
         mov     esi, esp
 count:  cmp byte ptr[esi], 0
         je      count1
-        inc     eax         ;count the switches that are set
+        inc     eax             ;count the switches that are set
 count1: inc esi
         loop    count
         nop
-        mov     esp, edx    ;restore stack pointer
+        mov     esp, edx        ;restore stack pointer
         ret
 light_switch endp
 switchloop: 
-        add     edi, eax            ;next switch
+        add     edi, eax        ;next switch
         cmp     edi, edx
         jae     @F
         xor byte ptr[edi], 1    ;toggle one switch
         jmp     switchloop  
-@@:
-        ret
+@@:     ret
         end
